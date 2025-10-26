f176058537b8c7390ec31b928fbaf0deb5a8eaa3
iomonad <iomonad@riseup.net>
2025-10-26 12:05:40 +0100

    release: 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.trosa/toolbox "1.0.0"
+(defproject io.trosa/toolbox "1.1.0"
   :description "Utils and Algorithms used on daily basis"
   :url "https://github.com/iomonad/toolbox"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -13,7 +13,7 @@
                                       :sign-releases false}]]
   :min-lein-version "2.5.3"
   :dependencies [[org.clojure/clojure "1.12.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.4.4"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.5.0"]]
                    :source-paths ["dev"]}
              :uberjar {:aot :all}}
   :aliases {"ci"      ["test"]}
