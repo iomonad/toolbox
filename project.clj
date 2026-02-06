41e3f364d66b3c780d336fdebc07c3cc45ed432f
iomonad <iomonad@riseup.net>
2026-02-06 18:29:10 +0100

    chore:lein upgraded deps

--- project.clj
+++ project.clj
@@ -13,8 +13,9 @@
                                       :sign-releases false}]]
   :min-lein-version "2.5.3"
   :dependencies [[org.clojure/clojure "1.12.4"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.5.0"]]
-                   :source-paths ["dev"]}
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.5.1"]]
+                   :source-paths ["dev"]
+                   :plugins [[lein-ancient "1.0.0-RC3"]]}
              :uberjar {:aot :all}}
   :aliases {"ci" ["test"]}
   :repl-options {:init-ns user})
